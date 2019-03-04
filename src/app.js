362760ee96daf956e77e6a463085624ddb4b8f0a
peter heinum <peter.heinum@gmail.com>
2019-03-04 15:03:48 +0100

    added get forecast function

--- src/app.js
+++ src/app.js
@@ -4,37 +4,104 @@ const express = require('express')();
 const mapquest_key = process.env.MAPQUEST_KEY;
 const mapquest_secret = process.env.MAPQUEST_SECRET;
 const darksky_key = process.env.DARKSKY_KEY;
-const port = process.env.PORT;
-express.listen(port);
+// const port = process.env.PORT;
+// express.listen(port);
+express.listen(3000);
+console.log('3000 active');
 
 express.get('/test', (req, res) => {
     res.send('Hola chica');
 })
 
-express.get('/api/:city', (req, res) => {
-    let city = req.param('city');
+// let city = 'stockholm';
+// reportWeatherFromCity(city)// , res)
+
+express.get('/', (req, res) => {
+    let city = 'stockholm';
     reportWeatherFromCity(city, res)
 })
-
-function reportWeatherFromCity(city, res) {
-    if(!checkIfStringContainsForbiddenSigns(city)){
-        let currentweather;
-        fetchLatAndLong(city).then(response => response.json()).then(latData => {
-            fetchWeather(getLatAndLngFromRes(latData)).then(response => response.json()).then(weatherData => {
-                currentweather = `${weatherData.currently.summary}, ${weatherData.currently.temperature}°C`;
-                res.send(currentweather);
-            });
-        });
+express.get('/api/currently/:city', (req, res) => {
+    let city = req.query('city');
+    if (!checkIfStringContainsForbiddenSigns(city)) {
+        reportCurrentWeatherFromCity(city, res)
     }
     else {
         res.send('Forbidden: City cannot contain å ä ö');
     }
+})
+
+
+express.get('/api/forecast/:city', (req, res) => {
+    let city = req.param('city');
+    reportForecastFromCity(city, res);
+})
+
+
+
+function reportCurrentWeatherFromCity(city, res) {
+    let weather;
+    let currentweather;
+    try {
+        fetchLatAndLong(city).then(response => response.json()).then(latData => {
+            fetchWeather(getLatAndLngFromRes(latData)).then(response => response.json()).then(weatherData => {
+                let sunsetTime = convertUnixToTime(weatherData.daily.data[0].sunsetTime);
+                let sunriseTime = convertUnixToTime(weatherData.daily.data[0].sunriseTime);
+                // console.log(sunriseTime + " sunrise")
+                // console.log(sunsetTime + " sunset")
+                weather = {
+                    windSpeed: weatherData.currently.windSpeed,
+                    summary: weatherData.currently.summary,
+                    Temperature: weatherData.currently.temperature,
+                    Humidity: weatherData.currently.humidity,
+                    sunrise: sunriseTime,
+                    sunset: sunsetTime
+                }
+                res.send(JSON.stringify(weather))
+                currentweather = `${weatherData.currently.summary}, ${weatherData.currently.temperature}°C`;
+                //res.send(currentweather);
+            });
+        });
+    } catch (error) {
+        let jsonError = JSON.stringify({ error: error.msg })
+        res.send(jsonError);
+    }
 }
 
-function checkIfStringContainsForbiddenSigns(string){
+function reportForecastFromCity(city, res) {
+    console.log("hello");
+    let report = [];
+    fetchLatAndLong(city).then(response => response.json()).then(latData => {
+        fetchWeather(getLatAndLngFromRes(latData)).then(response => response.json()).then(weatherData => {
+            let sunsetTime = convertUnixToTime(weatherData.daily.data[0].sunsetTime);
+            let sunriseTime = convertUnixToTime(weatherData.daily.data[0].sunriseTime);
+            //console.log(weatherData.daily.data);
+            let number = 0;
+            weatherData.daily.data.forEach(e => {
+                let template = {
+                    dayNr: number,
+                    windSpeed: e.windSpeed,
+                    summary: e.summary,
+                    temperatureMin: e.temperatureMin,
+                    temperatureMax: e.temperatureMax,
+                    humidity: weatherData.currently.humidity,
+                    apparentTemperatureMin: e.apparentTemperatureMin,
+                    apparentTemperatureMax: e.apparentTemperatureMax,
+                    sunrise: sunriseTime,
+                    sunset: sunsetTime
+                }
+                number++;
+                report.push(template);
+            });
+            res.send(JSON.stringify(report));
+
+        });
+    });
+}
+
+function checkIfStringContainsForbiddenSigns(string) {
     let strArr = [...string];
     strArr.forEach(letter => {
-        if(letter.toLowerCase() == 'å' || letter.toLowerCase() == 'ä' || letter.toLowerCase() == 'ö') {
+        if (letter.toLowerCase() == 'å' || letter.toLowerCase() == 'ä' || letter.toLowerCase() == 'ö') {
             return true;
         }
     });
@@ -57,4 +124,11 @@ function fetchLatAndLong(city) {
 
 function fetchWeather(latlng) {
     return fetch(`https://api.darksky.net/forecast/${darksky_key}/${latlng}?lang=sv&units=si`);
+}
+
+function convertUnixToTime(unix_time) {
+    let date = new Date(unix_time * 1000);
+    let hours = date.getHours();
+    let minutes = "0" + date.getMinutes();
+    return hours + ':' + minutes.substr(-2);
 }
